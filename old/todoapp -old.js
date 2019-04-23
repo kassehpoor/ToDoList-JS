54dbbc56ecf6b1153db6c72290eb7d40b78a095a
kassehpoor <kasehpoor.k@gmail.com>
2019-04-23 13:54:31 +0430

    refactor for MVC and add css file

---
+++ old/todoapp -old.js
@@ -0,0 +1,132 @@
+// version 0.0.2  save data on loca storage
+(function todoModule() {
+
+	var todos, addButton, deleteButton, todoText, toDoListElements;
+
+	window.onload = init;
+
+	function init() {
+		getTodosFromLocalStorage();
+
+		addButton = document.getElementById("addButton");
+		deleteButton = document.getElementById("deleteButton");
+		todoText = document.getElementById("todoText");
+		toDoListElements = document.getElementById("toDoListElements");
+
+		addButton.onclick = function () { addelement(todos) };
+		deleteButton.onclick = function () { deleteAllTodos(todos) };
+
+		render();
+	}
+	
+	//---------------------------------------------------------------
+
+	function deleteAllTodos(todos) {
+
+		[].concat(todos).forEach(function (todo) {
+			deleteElement(todo);
+		});
+
+		saveTodosOnLocalStorage();
+
+		render();
+	}
+
+	//----------------------------------------------------------------
+
+	function deleteElement(todo) {
+
+		todo.element.remove();
+		todos.splice(todos.indexOf(todo), 1);
+		saveTodosOnLocalStorage();
+		render();
+	}
+
+	//------Getting Todos elements form Loca Storage------------------------
+
+	function getTodosFromLocalStorage() {
+		var retrievedData = localStorage.getItem('todos');
+		todos = retrievedData ? JSON.parse(retrievedData) : [];
+		//window.todos = todos;
+	}
+
+	//-------Save Todos elements on Loca Storage----------------------------
+
+	function saveTodosOnLocalStorage() {
+		var todosToBeSaved = todos.map(todo => ({
+			text: todo.text,
+			isDone: todo.isDone
+		}));
+		localStorage.setItem('todos', JSON.stringify(todosToBeSaved));
+	}
+
+	//---------------------------------------------------------------------
+
+	function render() {
+		todos.forEach(function (todo) {
+			if (todo.isRendered) return;
+
+			var element = document.createElement("li");
+
+			element.innerHTML = todo.text;
+
+			toDoListElements.appendChild(element);
+			element.appendChild(createButtons(element, todo));
+
+			todo.isRendered = true;
+			todo.element = element;
+		});
+	}
+
+	//------------------------------------------------------------------
+
+	function addelement(todos) {
+		var el = document.getElementById("todoText");
+
+		if (el.value != "") {
+			todos.push({ text: el.value, isDone: false, isRendered: false });
+			saveTodosOnLocalStorage();
+			el.value = "";
+			el.focus();
+		}
+
+		render();
+	}
+
+	//-----------------------------------------------------------------
+
+	function createButtons(element, todo) {
+		var div = document.createElement("div");
+		div.style.display = 'inline';
+		var btnRemove = document.createElement("input");
+		btnRemove.type = "Button";
+		btnRemove.value = "delete";
+		btnRemove.onclick = function () {
+			deleteElement(todo);
+		};
+		div.appendChild(btnRemove);
+
+		var btndone = document.createElement("input");
+		btndone.type = "Button";
+		element.style.color = todo.isDone ? 'green' : 'gray';
+		btndone.style.backgroundColor = todo.isDone ? 'green' : 'gray';
+		btndone.value = todo.isDone ? 'unDone' : 'done';
+
+		btndone.addEventListener("click", function () {
+			//todo.isDone = todo.isDone ?false :true;
+			todo.isDone = !todo.isDone;
+			element.style.color = todo.isDone ? 'green' : 'gray';
+			btndone.style.backgroundColor = todo.isDone ? 'green' : 'gray';
+			btndone.value = todo.isDone ? 'unDone' : 'done';
+			saveTodosOnLocalStorage();
+		}, false);
+		div.appendChild(btndone);
+
+
+		return div;
+	}
+
+
+
+})();
+
--- todoapp.js
+++ todoapp.js
@@ -1,131 +1,55 @@
-// version 0.0.2  save data on loca storage
-(function todoModule() {
+//-----------------------model-
 
-	var todos, addButton, deleteButton, todoText, toDoListElements;
-	
-	window.onload = init;
-	
-	function init () {
-		getTodosFromLocalStorage();
+var todos = [];
+//--------------------controller------------------------------------------------------------------
+function addTodo(value) {
+	todos.push({ title: value, complete: false });
+	render();
+}
 
-		addButton = document.getElementById("addButton");
-	    deleteButton = document.getElementById("deleteButton");
-        todoText = document.getElementById("todoText");
-        toDoListElements = document.getElementById("toDoListElements");
-		
-		addButton.onclick = function (){addelement(todos)};
-	    deleteButton.onclick = function (){deleteAllTodos(todos)};
+function toggleComplete(todo) {
+	todo.complete = !todo.complete;
+}
 
-		render();
-	}
-	
-	//------Getting Todos elements form Loca Storage------------------------
-	
-	function getTodosFromLocalStorage() {
-		var retrievedData = localStorage.getItem('todos');
-		todos = retrievedData ? JSON.parse(retrievedData) : [];
-		//window.todos = todos;
-	}
-	
-	//-------Save Todos elements on Loca Storage----------------------------
-	
-	function saveTodosOnLocalStorage() {
-		var todosToBeSaved = todos.map(todo => ({
-			text: todo.text,
-			isDone: todo.isDone
-		}));
-		localStorage.setItem('todos', JSON.stringify(todosToBeSaved));
-	}
-	
-	//---------------------------------------------------------------------
+function remove(todo) {
+	// remove todo
+	render();
+}
 
-    function render() {
-		todos.forEach(function(todo) {
-			if(todo.isRendered) return;
-			
-			var element  = document.createElement("li");
-			
-            element.innerHTML = todo.text;
-						
-			toDoListElements.appendChild(element);
-			element.appendChild(createButtons(element, todo));
-			
-			todo.isRendered = true;
-			todo.element = element;
-		});
-    }
+//------------------------view------------------------------------------------------------------
+var todoListEl = document.getElementById('todoList');
 
-	//------------------------------------------------------------------
-	
-    function addelement(todos) {
-        var el = document.getElementById("todoText");
-		
-			if (el.value!=""){
-				todos.push({text:el.value,isDone:false,isRendered:false});
-				saveTodosOnLocalStorage();
-				el.value = "";
-				el.focus();
-			}
-		
-        render();
-    }
-	//---------------------------------------------------------------
+function render() {
+	todoListEl.innerHTML = '';
+	todos.forEach(function (todo) {
+		todoListEl.appendChild(createLi(todo));
+	});
 
-	function deleteAllTodos (todos) {
+	function createLi(todo) {
+		var li = document.createElement('li');
+		var span = document.createElement('span');
+		span.textContent = todo.title;
+		if (todo.complete) {
+			span.className = 'complete';
+		}
+		li.appendChild(span);
+		li.appendChild(createButtons(todo));
 		
-		[].concat(todos).forEach(function(todo){
-			deleteelement(todo);
-		});
-		
-		saveTodosOnLocalStorage();
-		
-		render();
-	}
-	
-	//----------------------------------------------------------------
-	
-	function deleteelement(todo) {
-		
-		todo.element.remove();
-		todos.splice(todos.indexOf(todo), 1);
-		saveTodosOnLocalStorage();
-		render();
+		return li;
 	}
 
-	//-----------------------------------------------------------------
-	
-	function createButtons(element,todo){
-		var div = document.createElement("div");
+	function createButtons(todo) {
+		var div = document.createElement('div');
+		var btnComplete = document.createElement('button');
+		var btnRemove = document.createElement('button');
+		
+		btnComplete.textContent = todo.complete ? 'undo' : 'do';
+		btnRemove.textContent = 'X';
+
 		div.style.display = 'inline';
-		var btnRemove = document.createElement("input");
-		btnRemove.type="Button";
-		btnRemove.value = "delete";
-		btnRemove.onclick=function(){
-			deleteelement(todo);
-		};
+		div.appendChild(btnComplete);
 		div.appendChild(btnRemove);
-		
-		var btndone = document.createElement("input");
-		btndone.type="Button";
-		element.style.color = todo.isDone ?'green':'gray';
-		btndone.style.backgroundColor = todo.isDone ?'green':'gray';
-		btndone.value = todo.isDone ? 'unDone' : 'done';
-		
-		btndone.addEventListener("click", function() {
-			//todo.isDone = todo.isDone ?false :true;
-			todo.isDone = !todo.isDone ;
-			element.style.color = todo.isDone ?'green':'gray';
-			btndone.style.backgroundColor = todo.isDone ?'green':'gray';
-			btndone.value = todo.isDone ? 'unDone' : 'done';
-			saveTodosOnLocalStorage();
-		}, false);
-		div.appendChild(btndone);
-		
-		
+
 		return div;
 	}
-	
-	
-	
-})();
-
+}
